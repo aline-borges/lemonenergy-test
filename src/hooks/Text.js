efe771a8946faaf5f7fdd76f384411d8a6dc11eb
Aline Borges <ap.borges@live.com>
2020-11-28 07:36:22 -0300

    refactor(removal of margin-left): remove margin-left from name and description components
    I removed the margin-left properties from the Body 200 and Title200 components to put the margin in
    the Info component that involves both components.

--- src/hooks/Text.js
+++ src/hooks/Text.js
@@ -17,6 +17,7 @@ export const Body200 = styled.p(
     font-size: 0.88rem;
     line-height: 1.5rem;
     letter-spacing: 0.02rem;
+    margin-left: 0.2rem;
   `,
 )
 
@@ -33,6 +34,7 @@ export const Title200 = styled.h1(
     font-weight: 700;
     font-size: 1.5rem;
     line-height: 2rem;
+    margin-left: 0.2rem;
   `,
 )
 
